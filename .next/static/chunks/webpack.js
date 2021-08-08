291542cde30105ef789c0607e0683c98fe9f9cea
agwenchez <agweraenock76@gmail.com>
2021-08-08 19:41:43 +0300

    UI bug fixes

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -182,7 +182,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "642167033021072604a4"; }
+/******/ 		__webpack_require__.h = function() { return "b5c90fa8512ce3c4adc7"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
---
+++ src/Components/Products.jsx
@@ -0,0 +1,55 @@
+import React, { useEffect, useState } from 'react'
+import Product from './Product'
+
+
+const Products = ({products}) => {
+    // const [products, setProducts] = useState([])
+
+    // useEffect(() => {
+    //    async function getProducts (){
+    //         const products = await fetch('https://fakestoreapi.com/products').then(
+    //         res => res.json()
+    //         ).catch(err => console.log("Error", err))
+            
+    //         setProducts(products)
+    //         // console.log("Products", products)
+    //    }
+    //    getProducts()
+    // }, [])
+    return (
+        <div className="grid grid-flow-row-dense md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 md:-mt-52 mx-auto">
+            {/* <h2>Products</h2> */}
+            {products.slice(0,4).map(({id, image, title, price, description, category}) =>(
+                <Product
+                 key={id}
+                 id={id}
+                 image={image}
+                 title={title}
+                 price={price}
+                 description={description}
+                 category={category}
+                />
+            ))}
+
+            <img className="md:col-span-full ml-5 pr-10 md:h-screen lg:h-64 w-full rounded-md" src="https://links.papareact.com/dyz" alt="advert" />
+
+            {/* <div> */}
+                {products.slice(4,products.length).map(({id, image, title, price, description, category}) =>(
+                    <Product
+                    key={id}
+                    id={id}
+                    image={image}
+                    title={title}
+                    price={price}
+                    description={description}
+                    category={category}
+                    />
+                ))}
+
+            {/* </div> */}
+        </div>
+    )
+}
+
+export default Products
+
