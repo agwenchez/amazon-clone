e96ad85974573c0d03b42fd0e1e0400f945d5bf6
agwenchez <agweraenock76@gmail.com>
2021-08-08 19:55:52 +0300

    UI bug fixes

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -182,7 +182,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "48aa66cdd3e65f832cff"; }
+/******/ 		__webpack_require__.h = function() { return "a4a4728aa30c2fcd0be9"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
